0d0374b3bab869a3713744921a8935d22f3d7e63
enisdenjo <badurinadenis@gmail.com>
2020-09-30 22:20:32 +0200

    remove unnecessary

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -17,12 +17,6 @@ local on_attach = function(client, bufnr)
   vim.fn.nvim_set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", {noremap = true, silent = true})
 end
 
-nvim_lsp.clangd.setup({
-  on_attach = on_attach,
-})
-nvim_lsp.pyls.setup{
-  on_attach = on_attach,
-}
 nvim_lsp.vimls.setup{
   on_attach = on_attach,
 }
@@ -33,59 +27,6 @@ nvim_lsp.html.setup{
   on_attach = on_attach,
 }
 nvim_lsp.cssls.setup{
-  on_attach = on_attach,
-  settings = {
-    css = {
-      validate = false
-    },
-    less = {
-      validate = true
-    },
-    scss = {
-      validate = true
-    }
-  }
-}
-
-local server_name = "svelte_lsp"
-local bin_name = "svelteserver"
-
--- local installer = util.npm_installer {
---   server_name = server_name,
---   packages = {"svelte-language-server"},
---   binaries = {bin_name}
--- }
-
-configs[server_name] = {
-  default_config = {
-    cmd = {bin_name, "--stdio"};
-    filetypes = {"svelte"};
-    root_dir = util.root_pattern("package.json", "tsconfig.json", ".git");
-   -- docs = {
-   --   description = [[
-   --   https://github.com/theia-ide/typescript-language-server
-   --   `typescript-language-server` can be installed via `:LspInstall tsserver` or by yourself with `npm`:
-   --   ```sh
-   --   npm install -g typescript-language-server
-   --   ```
-   --   ]];
-   --   default_config = {
-   --     root_dir = [[root_pattern("package.json", "tsconfig.json", ".git")]];
-   --   };
-   -- };
- };
-}
-
-nvim_lsp.sorbet.setup{
-  on_attach = on_attach,
-}
-nvim_lsp.svelte_lsp.setup{
-  on_attach = on_attach,
-}
-nvim_lsp.rust_analyzer.setup{
-  on_attach = on_attach,
-}
-nvim_lsp.elixirls.setup{
   on_attach = on_attach,
 }
 nvim_lsp.gopls.setup{
